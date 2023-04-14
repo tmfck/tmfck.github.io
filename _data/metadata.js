ac690dbc3455c8b895b310b93c6457b419286d72
tmfck <wa7619rz1@mozmail.com>
2023-04-14 23:01:52 +0700

    Initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "ARK17",
+	url: "https://ark17.x26.ch/",
+	language: "en",
+	description: "Lorem ipsum dolor sit amet",
+	author: {
+		name: "Tim",
+		email: "wa7619rz1@mozmail.com",
+		url: "https://ark17.x26.ch/about-me/"
+	}
+}
